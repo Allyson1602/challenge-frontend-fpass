b4b5b1e9772cc0098cef8b5b5d8b5c68fe8face8
Allyson1602 <aathyrson@yahoo.com.br>
2023-06-26 18:46:07 -0300

    feat: criação do layout para todas as páginas

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,14 +1,27 @@
 import { FC, PropsWithChildren } from "react";
 import styled from "styled-components";
 import BackgroundImage from "../assets/images/background.jpg";
+import MarvelLogo from "../assets/images/logo-marvel.png";
 
 export const LayoutStyled = styled.div`
-    background-image: url(${BackgroundImage});
+    width: 100vw;
+    height: 100vh;
+    background-image:  linear-gradient(to top, #000000BB, #000000F8, #000000BB), url(${BackgroundImage});
+    background-size: contain;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 16px;
+    box-sizing: border-box;
 `;
 
 const LayoutPage: FC<PropsWithChildren> = ({ children }) => {
     return (
         <LayoutStyled>
+            <img src={MarvelLogo} alt="Logo da Marvel" width="300px" />
+
             {children}
         </LayoutStyled>
     )
